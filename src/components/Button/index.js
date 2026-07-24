d3f7ddb78cc66d5bba424429c283dd784c5312b7
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2026-07-24 00:24:43 -0300

    Adicionado iterações com os botões numéricos

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import { ButtonContainer } from './styles';
 
 const Button = ({ label, onClick }) => {
     return (
-        <ButtonContainer onCLick={onClick}>
+        <ButtonContainer onClick={onClick}>
             { label }
         </ButtonContainer>
     );
